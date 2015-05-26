b48a7444b0a726dbb30b4775c8250dc6fa7dfd1f
Nigel Baillie <metreckk@gmail.com>
2015-05-26 15:11:13 -0400

    Update xmlparser.rb

--- lib/xsd/xmlparser.rb
+++ lib/xsd/xmlparser.rb
@@ -64,7 +64,7 @@ loaded = false
     # XXX: MUST BE REMOVED IN THE FUTURE
     name = lib.sub(/^.*\//, '')
     raise LoadError unless XSD::XMLParser.constants.find { |c|
-      c.to_s.downcase == name
+      c.to_s.downcase == name.to_s
     }
     loaded = true
     break
